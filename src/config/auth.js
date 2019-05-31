d77da96bb45fece2a7a4b9d7eda2f6d50754dd47
Giuppi <g.funicello@gmail.com>
2019-05-31 09:54:41 +0200

    add bearer string to auth

--- src/config/auth.js
+++ src/config/auth.js
@@ -4,7 +4,7 @@ const authentication = (secret) => {
   const getTokenFromHeaders = (req) => {
   const { headers: { authorization } } = req;
 
-    if(authorization && authorization.split(' ')[0] === 'Token') {
+    if(authorization && authorization.split(' ')[0] === 'Bearer') {
       return authorization.split(' ')[1];
     }
     return null;
